dc54018d724d297d4e62c6e9657a6d07bb771f46
Dmitry B <oxitnik@gmail.com>
2020-02-15 22:48:24 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const { SSMClient } = require("@aws-sdk/client-ssm-node/SSMClient");
+const { GetParameterCommand } = require('@aws-sdk/client-ssm-node/commands/GetParameterCommand');
+
+
+async function getParameterValue(client, paramName) {
+    const getParameterCommand = new GetParameterCommand({
+        Name: paramName,
+        WithDecryption: true
+    })
+
+    const response = await client.send(getParameterCommand)
+    const value = (response.Parameter || {}).Value
+
+    return value
+}
+
+
+module.exports.templateTags = [{
+    name: 'aws_ssm_param',
+    displayName: 'AWS SSM Parameter',
+    description: 'AWS SSM Parameter',
+    args: [
+        {
+            displayName: 'Parameter name',
+            description: 'Parameter name',
+            type: 'string'
+        }
+    ],
+    async run (context, paramName) {
+        const ssmClient = new SSMClient({})
+        const paramValue = await getParameterValue(ssmClient, paramName)
+
+        return paramValue
+    }
+}];
